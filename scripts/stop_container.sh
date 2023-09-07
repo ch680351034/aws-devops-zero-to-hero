19f0098ebf211394a2a46f047ffd52652716ab36
ch680351034 <37712815+ch680351034@users.noreply.github.com>
2023-09-07 17:46:33 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+echo "hello................."
 # Delete container by ID  
-con=$(docker ps | awk '{print $1}'|tail -1)
+con=`docker ps | awk '{print $1}'|tail -1`
 docker rm -f $con
